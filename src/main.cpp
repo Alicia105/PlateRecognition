54c580a38f95dbc9cf93497cadb75291fcc020b8
Alicia105 <aliciacande105@gmail.com>
2025-05-15 09:58:41 -0500

    fix: Fixed out of bound matrix assertion

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <bits/stdc++.h>
 #include <opencv2/opencv.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/videoio.hpp>
 #include <opencv2/core.hpp>
 #include <opencv2/video/tracking.hpp>
 #include "../include/inference.h"
@@ -13,6 +15,26 @@ using namespace std;
 using namespace sort;
 
 const string alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+
+string generateName(){
+    time_t timestamp = time(nullptr);
+    struct tm datetime;
+
+    if (localtime_s(&datetime, &timestamp) != 0) {
+        cerr << "ERROR: localtime_s() failed"<<endl;
+    }
+
+    char output[50];
+    size_t count = strftime(output, sizeof(output), "%d-%m-%Y_%H-%M-%S", &datetime);
+    if (count == 0) {
+        cerr << "ERROR: strftime() failed"<<endl;
+    }
+
+    cout << "Formatted time: " << output << endl;
+
+    string name = "../results/output_" + string(output)+ ".mp4";
+    return name;
+}
 
 // Preprocess the plate image to match model input
 cv::Mat preprocessPlate(const cv::Mat& plate) {
@@ -81,6 +103,7 @@ vector<string> getClassNames(string filePath){
 int main() {
 
     bool runOnGPU = false;
+    bool saveVideo = true;
     string pathToVideo="../videos/2103099-uhd_3840_2160_30fps.mp4";
     string pathToCarModel="../models/yolov8n.onnx";
     string pathToPlateModel="../models/yolov8n_plate.onnx";
@@ -88,10 +111,8 @@ int main() {
     string pathToOcr="../models/global_mobile_vit_v2_ocr.onnx";
     string classNameFilePath="../models/coco.names";
     string classPlateFilePath="../models/plate.names"; 
-    string ocrCharacterFilePath="../models/labels.txt"; 
 
     vector<string> allClasses=getClassNames( classNameFilePath);
-    vector<string> allChar=getClassNames(ocrCharacterFilePath);
     vector<string> wantedClasses={"person","bicycle","car","motorbike","bus","truck"}; 
     vector<cv::Scalar> savedColors;
 
@@ -103,13 +124,21 @@ int main() {
         cerr << "Error: Could not open video stream."<<endl;
         return -1;
     }
-
+    
     Inference inf(pathToCarModel, cv::Size(640, 640), classNameFilePath, runOnGPU);
     Inference infPlate(pathToPlateModel, cv::Size(640, 640), classPlateFilePath, runOnGPU);
 
     float width=cap.get(cv::CAP_PROP_FRAME_WIDTH);
     float height=cap.get(cv::CAP_PROP_FRAME_HEIGHT);
     float fps=cap.get(cv::CAP_PROP_FPS);
+
+    string nameFile=generateName();
+
+    cv::VideoWriter writer(nameFile, cv::VideoWriter::fourcc('M','J','P','G'),fps,cv::Size(width, height));
+    if (!writer.isOpened()) {
+        cerr << "Error: Cannot open the output file to write" << endl;
+        saveVideo=false;
+    }
 
     int resized_width = 1280;
     int  resized_height = 720;
@@ -121,11 +150,11 @@ int main() {
     int numberOfFrame=0;
 
     float plateConfidenceThreshold=0.5;
-    double IoUThreshold=0.5;
+    float confidenceThreshold=0.5;
 
     Sort::Ptr sortTracker = make_shared<Sort>(1, 3, 0.3f);
 
-    while (cap.isOpened()) {
+    while (cap.isOpened()){
 
         cv::Mat frame,resizedFrame,detectionsMat;;
         cap >> frame;
@@ -148,7 +177,7 @@ int main() {
             Detection detection = output[i];
 
             for(auto name : wantedClasses){
-                if (name==detection.className){
+                if (name==detection.className && detection.confidence>plateConfidenceThreshold){
 
                     cv::Rect box=detection.box;
                     float center_x =box.x+box.width/2;
@@ -173,7 +202,6 @@ int main() {
         //Update tracker
         cv::Mat trackedObjects = sortTracker->update(detectionsMat);
         //update output : bounding boxes estimate: [xc, yc, w, h, score, class_id, vx, vy, tracker_id]
-        vector<int> validTrackerId;
 
         for (int j = 0; j < trackedObjects.rows; ++j) {
 
@@ -203,10 +231,21 @@ int main() {
             cv::rectangle(resizedFrame, textBox, color, cv::FILLED);
             cv::putText(resizedFrame, classString, cv::Point(bbox.x + 5, bbox.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
 
-            //Plate detection
-            cv::Mat imagePlate=resizedFrame(bbox);
-            vector<Detection> outputPlate = infPlate.runInference(imagePlate);
-            int plates=outputPlate.size();
+            // Ensure ROI is within image bounds
+            int plates;
+            vector<Detection> outputPlate;
+            bbox &= cv::Rect(0, 0, resizedFrame.cols, resizedFrame.rows);
+
+            if (bbox.width > 0 && bbox.height > 0) {
+                //Plate detection
+                cv::Mat imagePlate=resizedFrame(bbox);//safe crop
+                outputPlate = infPlate.runInference(imagePlate);
+                plates=outputPlate.size();
+            }
+            else{
+                continue;
+            }
+
 
             for(int k=0; k<plates; ++k){
                 Detection plateDetection=outputPlate[k];
@@ -220,35 +259,36 @@ int main() {
                 plateBox.y+=bbox.y;
 
                 //Plate Detection box
-                cv::rectangle(resizedFrame, plateBox, plateColor, 2);
-                        
-                //OCR in Plate Detection box
-                cv::Mat plateCropped = resizedFrame(plateBox); // From plate detection
-                string text = recognizePlate(ocrNet, plateCropped,alphabet);
+                cv::rectangle(resizedFrame, plateBox, plateColor, 2);                        
+                
+                if(plateDetection.confidence>0.7){
+                    //OCR in Plate Detection box 
+                    cv::Mat plateCropped = resizedFrame(plateBox);
+                    string text = recognizePlate(ocrNet, plateCropped,alphabet);
 
-                /*string classStringPlate = plateDetection.className + ' ' + to_string(plateDetection.confidence).substr(0, 4);
-                cv::Size textSizePlate = cv::getTextSize(classStringPlate, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
-                cv::Rect textBoxPlate(plateBox.x, plateBox.y - 40, textSizePlate.width + 10, textSizePlate.height + 20);*/
+                    //Draw plate detection + OCR
+                    cv::Size textSizePlate = cv::getTextSize(text, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
+                    cv::Rect textBoxPlate(plateBox.x, plateBox.y - 40, textSizePlate.width + 10, textSizePlate.height + 20);
+                    cv::rectangle(resizedFrame, textBoxPlate, plateColor, cv::FILLED);
+                    cv::putText(resizedFrame, text, cv::Point(plateBox.x + 5, plateBox.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
+                    cout<<text<<endl;
 
-                //Draw plate detection + OCR 
-                string classStringPlate = text;
-                cv::Size textSizePlate = cv::getTextSize(classStringPlate, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
-                cv::Rect textBoxPlate(plateBox.x, plateBox.y - 40, textSizePlate.width + 10, textSizePlate.height + 20);
+                }
 
-                cv::rectangle(resizedFrame, textBoxPlate, plateColor, cv::FILLED);
-                cv::putText(resizedFrame, classStringPlate, cv::Point(plateBox.x + 5, plateBox.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
-                cout<<text<<endl;
             }
       
             treated++;  
         }
-       
+
+        if(saveVideo) writer.write(resizedFrame);
+
         cv::imshow("Original",resizedFrame);
         int k = cv::waitKey(10); // Wait for a keystroke in the window
         if(k=='q'){break;}
     }
 
     cap.release();
+    writer.release();
     cv::destroyAllWindows();
 
     width=static_cast<int>(width);
@@ -261,208 +301,3 @@ int main() {
 
     return 0;
 }
-
-/*int main() {
-    cv::Mat m = cv::imread("../images/nevada.jpg");
-    string ocrCharacterFilePath="../models/labels.txt";
-    vector<string> allChar=getClassNames(ocrCharacterFilePath);
-
-    if (m.empty()) {
-        cerr << "Failed to load image\n";
-        return -1;
-    }
-
-    string pathToOcr = "../models/global_mobile_vit_v2_ocr.onnx";
-    cv::dnn::Net crnnNet = cv::dnn::readNetFromONNX(pathToOcr);
-    
-    vector<string> names = crnnNet.getLayerNames();
-
-    for (const auto& name : names) cout << name <<endl;
-    cout << "Input name: " << crnnNet.getUnconnectedOutLayersNames()[0] << std::endl;
-
-
-    string text = recognizePlate(crnnNet,m,alphabet);
-    cout << "Detected Plate: " << text << endl;
-
-    cv::imshow("Original", m);
-    int k = cv::waitKey(0);
-    return 0;
-}*/
-
-
-/*
-int main() {
-
-    bool runOnGPU = false;
-    string pathToVideo="../videos/2103099-uhd_3840_2160_30fps.mp4";
-    string pathToCarModel="../models/yolov8n.onnx";
-    string pathToPlateModel="../models/yolov8n_plate.onnx";
-    string classNameFilePath="../models/coco.names";
-    string classPlateFilePath="../models/plate.names"; 
-    vector<string> wantedClasses={"person","bicycle","car","motorbike","bus","truck"}; 
-    vector<TrackableObject> activeObjects;
-    
-    cv::VideoCapture cap(pathToVideo);
-
-    if (!cap.isOpened()) {
-        cerr << "Error: Could not open video stream."<<endl;
-        return -1;
-    }
-
-    Inference inf(pathToCarModel, cv::Size(640, 640), classNameFilePath, runOnGPU);
-    Inference infPlate(pathToPlateModel, cv::Size(640, 640), classPlateFilePath, runOnGPU);
-
-    float width=cap.get(cv::CAP_PROP_FRAME_WIDTH);
-    float height=cap.get(cv::CAP_PROP_FRAME_HEIGHT);
-    float fps=cap.get(cv::CAP_PROP_FPS);
-
-    int resized_width = 1280;
-    int  resized_height = 720;
-
-    int treated=0;
-    int dropped=0;
-
-    int id=0;
-    int numberOfFrame=0;
-
-    float plateConfidenceThreshold=0.5;
-    double IoUThreshold=0.5;
-
-    while (cap.isOpened()) {
-
-        cv::Mat frame,resizedFrame;
-        cap >> frame;
-        cv::resize(frame,resizedFrame,cv::Size(resized_width,resized_height));
-        numberOfFrame++;
-
-        if (frame.empty()) break;
-        if(numberOfFrame%5==0) continue;
-        
-        vector<Detection> output = inf.runInference(resizedFrame);
-        int detections = output.size();
-        cout << "Number of detections:" << detections <<endl;
-
-        //Detection
-        for(int i=0; i<detections; ++i){
-            Detection detection = output[i];
-
-            for(auto name : wantedClasses){
-                if (name==detection.className){
-                    
-                    //Tracking
-                    if(activeObjects.size()==0){
-                        TrackableObject newObj;
-                        newObj.tracker = cv::TrackerMIL::create();
-                        newObj.tracker->init(resizedFrame, detection.box);
-                        newObj.id =id++;
-                        newObj.box = detection.box;
-                        newObj.className = detection.className;
-                        newObj.color = detection.color;
-                        activeObjects.push_back(newObj);
-                        continue;
-                    }
-
-                    for (auto& obj : activeObjects) {
-                        cv::Rect b=obj.box;
-                        bool ok = obj.tracker->update(resizedFrame, b);
-                        if (!ok) obj.toRemove = true;
-                    }
-                    
-                    if (activeObjects.size()!=0) {
-                        bool matched = false;
-                        for (auto& obj : activeObjects) {
-                            double iou = computeIoU(detection.box, obj.box);
-                            if (iou > IoUThreshold) {
-                                matched = true;
-                                break;
-                            }
-                        }
-                        if (!matched) {
-                            TrackableObject newObj;
-                            newObj.tracker = cv::TrackerMIL::create();
-                            newObj.tracker->init(resizedFrame, detection.box);
-                            newObj.id = id++;
-                            newObj.box = detection.box;
-                            newObj.className = detection.className;
-                            newObj.color = detection.color;
-                            activeObjects.push_back(newObj);
-                        }
-                    }
-                    
-                    // Remove lost trackers
-                    activeObjects.erase(
-                        remove_if(activeObjects.begin(), activeObjects.end(),
-                                       [](const TrackableObject& obj) { return obj.toRemove; }),
-                        activeObjects.end()
-                    );
-
-                }
-            }
-            
-            for (auto& obj : activeObjects){
-                cv::Rect box = obj.box;
-                cv::Scalar color = obj.color;
-
-                // Detection box
-                cv::rectangle(resizedFrame, box, color, 2);
-
-                // Draw Detection box text
-                string classString = obj.className + ' ' + to_string(obj.confidence).substr(0, 4);
-                cv::Size textSize = cv::getTextSize(classString, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
-                cv::Rect textBox(box.x, box.y - 40, textSize.width + 10, textSize.height + 20);
-
-                cv::rectangle(resizedFrame, textBox, color, cv::FILLED);
-                cv::putText(resizedFrame, classString, cv::Point(box.x + 5, box.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
-
-                //Plate detection
-                cv::Mat imagePlate=resizedFrame(box);
-                vector<Detection> outputPlate = infPlate.runInference(imagePlate);
-                int plates=outputPlate.size();
-
-                for(int j=0; j<plates; ++j){
-                    Detection plateDetection=outputPlate[j];
-
-                    if(plateDetection.confidence<plateConfidenceThreshold) continue;
-
-                    cv::Rect plateBox = plateDetection.box;
-                    cv::Scalar plateColor = color;
-                           
-                    plateBox.x+=box.x;
-                    plateBox.y+=box.y;
-
-                    // Detection box
-                    cv::rectangle(resizedFrame, plateBox, plateColor, 2);
-                    
-                    // Detection box text
-                    string classStringPlate = plateDetection.className + ' ' + to_string(plateDetection.confidence).substr(0, 4);
-                    cv::Size textSizePlate = cv::getTextSize(classStringPlate, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
-                    cv::Rect textBoxPlate(plateBox.x, plateBox.y - 40, textSizePlate.width + 10, textSizePlate.height + 20);
-
-                    cv::rectangle(resizedFrame, textBoxPlate, plateColor, cv::FILLED);
-                    cv::putText(resizedFrame, classStringPlate, cv::Point(plateBox.x + 5, plateBox.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
-
-                }
-
-            }
-                           
-            treated++;  
-        }
-
-        cv::imshow("Original",resizedFrame);
-        int k = cv::waitKey(10); // Wait for a keystroke in the window
-        if(k=='q'){break;}
-    }
-
-    cap.release();
-    cv::destroyAllWindows();
-
-    width=static_cast<int>(width);
-    height=static_cast<int>(height);
-    fps=static_cast<int>(fps);
-
-    cout<<"Frame : [width="<<width<<" x height="<<height<<"]"<<endl;
-    cout<<"FPS :"<<fps<<endl;
-    cout<<"Number of images ="<<numberOfFrame<<" , "<<"Treated ="<<treated<<" , "<<"Dropped ="<<dropped<<endl;
-
-    return 0;
-}*/
